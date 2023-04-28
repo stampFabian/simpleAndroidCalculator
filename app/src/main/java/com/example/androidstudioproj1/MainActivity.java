69084240396ad3931e5d22e70fad7978ac902656
stampFabian <fabian.trenkwalder@hak-kitz.ac.at>
2023-04-28 07:56:26 +0200

    fix: calculator works now

--- app/src/main/java/com/example/androidstudioproj1/MainActivity.java
+++ app/src/main/java/com/example/androidstudioproj1/MainActivity.java
@@ -2,11 +2,9 @@ package com.example.androidstudioproj1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,10 +19,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input = findViewById(R.id.input);
     }
 
     public void logic(View view) {
-        input = findViewById(R.id.input);
         btn_numb0 = findViewById(R.id.btn_numb0);
         btn_numb1 = findViewById(R.id.btn_numb1);
         btn_numb2 = findViewById(R.id.btn_numb2);
